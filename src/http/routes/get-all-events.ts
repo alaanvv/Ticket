61439d73ea6029486b714d9823e697fe545ef473
alaanvv <valealan84@gmail.com>
2024-06-27 21:57:52 -0300

    feat: added get-all-events route

---
+++ src/http/routes/get-all-events.ts
@@ -0,0 +1,10 @@
+import { FastifyInstance } from 'fastify'
+import { prisma } from '../../lib/prisma'
+
+export async function getAllEvents(app: FastifyInstance) {
+  app.get('/all-events', async (_, res) => {
+    const events = await prisma.event.findMany()
+
+    return res.status(200).send({ events })
+  })
+}
